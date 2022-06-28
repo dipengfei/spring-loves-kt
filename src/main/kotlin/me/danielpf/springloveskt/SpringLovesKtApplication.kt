169740eda91f22c86f8451880dc2f936a45fcbf0
dipengfei <pengfei.di@rakuten.com>
2022-06-28 21:00:08 +0800

    Initial commit

---
+++ src/main/kotlin/me/danielpf/springloveskt/SpringLovesKtApplication.kt
@@ -0,0 +1,11 @@
+package me.danielpf.springloveskt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringLovesKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringLovesKtApplication>(*args)
+}
